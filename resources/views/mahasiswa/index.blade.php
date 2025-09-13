8f850c50d734836e3801a4da97e8590450fb35c8
Nninaa135 <nina.lp3i.krw24@gmail.com>
2025-09-13 10:39:02 +0700

    'createMahasiswa'

---
+++ resources/views/mahasiswa/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h1>Tambah Mahasiswa</h1>
+    <form method="POST" action="/mahasiswa">
+        @csrf
+        <input type="text" name="nama" placeholder="Nama"><br>
+        <input type="text" name="nim" placeholder="NIM"><br>
+        <button type="submit">Simpan</button>
+    </form>
+
+    <h2>List Mahasiswa</h2>
+    <ul>
+        @foreach($data as $mhs)
+            <li>{{ $mhs->nama }} - {{ $mhs->nim }}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/mahasiswa',[MahasiswaController::class,'index']);
+Route::post('/mahasiswa',[MahasiswaController::class,'store']);
 
 Route::get('/', function () {
     return view('welcome');
